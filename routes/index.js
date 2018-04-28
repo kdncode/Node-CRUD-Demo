8bdb69de80bb90614aefc99716e49567c8d8de6c
kdncode <kdncode@gmail.com>
2018-04-28 15:17:56 -0500

    manipulated data from db to view page

--- routes/index.js
+++ routes/index.js
@@ -51,13 +51,39 @@ MongoClient.connect(url, function(err, client) {
   });
 });
 
-res.redirect('/new')
+res.redirect('/new');
+});
 
 /* Get View page */
-router.get("/view", (req, res, next) => {
-  res.render("view", { title: "View my data"})
-})
+router.get('/view', function(req, res, next) {
 
-});
+  // Find All Documents from https://github.com/mongodb/node-mongodb-native
+  const findDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('user');
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      console.log("Found the following records");
+      console.log(docs)
+      callback(docs);
+    });
+  }
+
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+
+    const db = client.db(dbName);
+
+      findDocuments(db, function(viewData) {
+        res.render('view', { title: 'View my data', userData : viewData} )
+        console.log(viewData)
+        client.close();
+      });
+    });
+  });
+
 
 module.exports = router;
